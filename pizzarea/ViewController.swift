a3213ae902360b6223bfeb5249c6cc0bb07f0873
Neo Ighodaro <neo@creativitykills.co>
2018-02-06 18:40:14 +0100

    Initial Commit

---
+++ pizzarea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pizzarea
+//
+//  Created by Neo Ighodaro on 06/02/2018.
+//  Copyright © 2018 CreativityKills Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
