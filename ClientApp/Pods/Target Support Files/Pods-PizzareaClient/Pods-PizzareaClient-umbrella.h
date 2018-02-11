b144bbafee7ad886b199e587bcfe058025fc7026
Neo Ighodaro <neo@creativitykills.co>
2018-02-11 09:26:13 +0100

    Updated the client app

--- ClientApp/Pods/Target Support Files/Pods-PizzareaClient/Pods-PizzareaClient-umbrella.h
+++ ClientApp/Pods/Target Support Files/Pods-PizzareaClient/Pods-PizzareaClient-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_pizzareaVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_pizzareaVersionString[];
+FOUNDATION_EXPORT double Pods_PizzareaClientVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PizzareaClientVersionString[];
 
